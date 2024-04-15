58e3f98b17439bebb7bf7e9126a2196aeb0d2370
jp <johnpwilkinson@outlook.com>
2024-04-15 10:50:26 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `jpw_portfolio`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
